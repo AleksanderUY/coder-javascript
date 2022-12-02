b6822f02749d73d2c41d6b756f602c9a1f297b5a
Aleksander Simanski Sanin <alesim428@gmail.com>
2022-12-02 11:36:41 -0300

    agregando la primera parte del script que es el saludo de bienvenida e ingreso de datos del estudiante

---
+++ main.js
@@ -0,0 +1,34 @@
+alert ("Le damos la bienvenida al sistema de ingreso de la Fing");
+
+// se le pregunta al estudiante si esta registrado
+/*const si = "si";
+const no = "no";
+prompt = ("Ya te registraste?");
+
+if (si) {
+    
+} else {
+    
+}*/
+
+const nombre = prompt ("Ingresa tu nombre");
+const apellido = prompt ("Ingresa tu apellido");
+const cedula = prompt ("Ingresa tu cedula sin puntos ni guiones");
+
+alert ("Bienvenido " + nombre);
+/*funtion ingresar() {
+let ingresar = false;
+
+for (let i = 0; i < i>=0; i++) {
+    const usuario= prompt ("Ingresa tu usuario");
+
+    if (usuario===cedula);
+    alert("Bienvenido a la Fing");
+    ingresar=true;
+}
+else {
+    alert ("no estas registrado, Ingresa tus datos por primera vez");
+
+
+}
+}*/
