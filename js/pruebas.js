c0caade9477511fb44a1ad4e177ea3973f56265b
Aleksander Simanski Sanin <alesim428@gmail.com>
2022-12-22 14:32:36 -0300

    Se agrega lo ultimo dado hasta la clase 6,array y filtrado.pruebas.js pasa a ser el script a revisar en la pre-entrega

---
+++ js/pruebas.js
@@ -0,0 +1,81 @@
+
+
+let si = "si";
+let no = "no";
+let despedida ()=> {
+  alert("Nos vemos pronto! Te saluda la Fing.")
+};
+//array con datos inventados
+const estudiantes = [
+  { cedula: 54607892, nombre: "juan", apellido: "perez" },
+  { cedula: 47916526, nombre: "Solana", apellido: "Rodriguez" },
+  { cedula: 54328708, nombre: "Natalia", apellido: "Garcia" },
+  { cedula: 40429520, nombre: "Mariana", apellido: "Martínez" },
+  { cedula: 45362513, nombre: "Adrian", apellido: "González" },
+  { cedula: 53111083, nombre: "Diego", apellido: "Sánchez" },
+  { cedula: 46280007, nombre: "Sebastian", apellido: "ponce" },
+  { cedula: 50542387, nombre: "alvaro", apellido: "castro" },
+];
+estudiantes.push(cedula,nombre,apellido);
+estudiantes.filter((cedula) => (cedula.nombre = "juan"));
+
+function ingreso(alert) {
+  alert("ya puedes ingresar  " + nombre);
+}
+
+
+alert("Le damos la bienvenida al sistema de ingreso de la Fing");
+
+
+// se le pregunta al estudiante si esta registrado
+let registro = prompt("Estas registrado?");
+if (registro === "si") {
+  let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+  if (cedula == "") {
+    alert("No ingresaste tu cedula");
+    let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+  }
+}
+//funcion de ingreso y salida a la Fing.
+function ingreso(E,S) {
+  if (E = true)
+  let entrada = prompt("Ingresas a la Fing?");
+}else {
+  let salida = prompt("Sales a la Fing?");
+  if (S){
+    let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+    return despedida();
+  }
+}
+// si el usuario digita no, pasa a rellenar los datos.
+if (registro === "no") {
+  alert("registrarte por favor para ingresar");
+  if (no) {
+    let nombre = prompt("Ingresa tu nombre");
+    if (nombre == "") {
+      alert("No ingresaste ningun nombre");
+      let nombre = prompt("Ingresa tu nombre");
+    }
+  }
+}
+
+  let apellido = prompt("Ingresa tu apellido");
+  if (apellido == "") {
+    alert("No ingresaste ningun apellido");
+    let apellido = prompt("Ingresa tu apellido");
+  }
+
+  let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+  if (cedula == "") {
+    alert("No ingresaste tu cedula");
+    let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+  }
+  ingreso("Disfruta tu estadia " + nombre + apellido);
+
+
+
+
+
+
+  
+  
