aa21eecd02f31cb7f33f12a41766d95570872c0a
Aleksander Simanski Sanin <alesim428@gmail.com>
2023-01-28 10:44:53 -0300

    agregando botones, imagenes,favicon,documentacion al proyecto

--- js/clase8.js
+++
@@ -1,9 +0,0 @@
-/* let temperatura = 31
-
-if (temperatura > 30) {
-    alert ("dia caluroso")
-} else {
-    alert ("dia agradable")
-} */
-temperatura > 30 ? alert("dia caluroso") : alert("dia agradable")
-
--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
-alert("Le damos la bienvenida al sistema de ingreso de la Fing");
-let si = "si";
-let no = "no";
-// se le pregunta al estudiante si esta registrado
-let registro = prompt("Estas registrado?");
-if (si === "si") {
-  let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
-  if (cedula == "") {
-    alert("No ingresaste tu cedula");
-    let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
-  }
+let estudiantes = []
+let estudiante = {nombreCompleto:"", cedula:"",institucion:""}
+
+
+function Botones() {
+  // Crear dos elementos de botón
+  this.botonEntrar = document.createElement("button");
+  this.botonSalir = document.createElement("button");
+  this.botonEntrar.setAttribute("id","entrada");
+  this.botonSalir.setAttribute("id","salida");
+  // Establecer el texto de los botones
+  this.botonEntrar.innerHTML = "Entrada";
+  this.botonSalir.innerHTML = "Salida";
+
+  // Agregar los botones al documento
+paso1.appendChild(this.botonEntrar);
+paso1.appendChild(this.botonSalir);
 }
-// si el usuario digita no pasa a rellenar los datos.
-if (no === "no") {
-  alert("registrarte por favor para ingresar");
-  if (no) {
-    let nombre = prompt("Ingresa tu nombre");
-    if (nombre == "") {
-      alert("No ingresaste ningun nombre");
-      let nombre = prompt("Ingresa tu nombre");
-    }
-  }
 
-  let apellido = prompt("Ingresa tu apellido");
-  if (apellido == "") {
-    alert("No ingresaste ningun apellido");
-    let apellido = prompt("Ingresa tu apellido");
-  }
+// Crear una nueva instancia de la función constructora
+var misBotones = new Botones();
 
-  let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
-  if (cedula == "") {
-    alert("No ingresaste tu cedula");
-    let cedula = prompt("Ingresa tu cedula sin puntos ni guiones");
+function ingresoFing(botonEntrar,botonSalir){
+  if(botonEntrar){
+    //desplegar el siguiente paso que seria ingresar o registrarse
+  } else if(botonSalir){
+    /* desplegar el formulario de salida 
+     */
   }
-  function ingreso() {
-    alert("ya puedes ingresar  " + apellido);
-  }
-  ingreso();
 }
